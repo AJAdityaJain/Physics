91e29b8cdcee531f13b0902a508cf51b23b516f4
Aditya Jain <93772697+AJAdityaJain@users.noreply.github.com>
2023-05-25 13:35:25 +0530

    Delete Physics/src directory

--- Physics/src/util/Config.java
+++
@@ -1,9 +0,0 @@
-package util;
-
-public class Config {
-	public static final float SHADOW = 4;
-	public static float FPS = 60f;
-	public static float FIELD = 100 * 10/FPS;
-	public static float G = 6.673f * 10/FPS;//* 10-11;
-	public static float C = 299.792458f * 10/FPS;;//* 10-11;
-}
